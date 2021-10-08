e5134dcb506e2b9bf4d5184f7d82ec8b4831996d
Adnan <adnan_abbasi@yahoo.com>
2021-10-08 16:33:38 -0400

    End of section 11

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: 'reventscourse-1b1e8',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
