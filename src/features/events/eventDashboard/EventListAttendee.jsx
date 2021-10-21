bc4689dd3ff925885ce31e14a87b94e5fa23a621
Adnan <adnan_abbasi@yahoo.com>
2021-10-20 21:02:40 -0400

    End of section 14

--- src/features/events/eventDashboard/EventListAttendee.jsx
+++ src/features/events/eventDashboard/EventListAttendee.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Image, List } from "semantic-ui-react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Image, List } from 'semantic-ui-react';
 
-export default function EventListAttendee({attendee}) {
+export default function EventListAttendee({ attendee }) {
   return (
-    <List.Item>
+    <List.Item as={Link} to={`/profile/${attendee.id}`}>
       <Image size='mini' circular src={attendee.photoURL} />
     </List.Item>
-  )
+  );
 }
