a0d21890a252eb9613e8c74be825b5c32b642a64
Adnan <adnan_abbasi@yahoo.com>
2021-10-22 13:27:35 -0400

    End of section 15

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: 'reventscourse-1b1e8',
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID
